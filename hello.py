9517579f0f3d1732fec16ef42d0448ce972316f1
Ali <a.garnii8@gmail.com>
2020-05-31 02:32:34 +0300

    hello world and multiline new line

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-print(123) 
-print('last')
+#this is a hello world application 
+
+print ('hello world!\n')
+print ('hello world! 2')
+# multiline 
+print ('''this is a multi line hello world , today is a very sunny''' )
